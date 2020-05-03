f959b4f19bc3c1ff3f26ca759cc54da947924656
NicoleKuong <nicolekuong@gmail.com>
2020-05-03 20:18:58 +0200

    apply connection and disconnection with socket

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
+const router = require("./router");
 
 const PORT = process.env.PORT || 4000;
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
+
+//run when there is a client connection
+//socket is going to be connected with the client side
+io.on("connection", (socket) => {
+  console.log("We have a new connection!");
+
+  socket.on("disconnect", () => {
+    console.log("User had left!");
+  });
+});
+
+app.use(router);
 
 server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
--- router.js
+++ router.js
@@ -5,3 +5,5 @@ const router = new Router();
 router.get("/", (req, res) => {
   res.send("server is up and running");
 });
+
+module.exports = router;

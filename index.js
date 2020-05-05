11365b943f3e16e17924d9fa0a52ff4bd858be91
NicoleKuong <nicolekuong@gmail.com>
2020-05-05 13:00:34 +0200

    create different functions for users

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const http = require("http");
 const socketIo = require("socket.io");
 const router = require("./router");
 
+const { addUser, removeUser, getUser, getUsersInRoom } = require("./users");
 const PORT = process.env.PORT || 4000;
 
 const app = express();
@@ -13,6 +14,11 @@ const io = socketIo(server);
 //socket is going to be connected with the client side
 io.on("connection", (socket) => {
   console.log("We have a new connection!");
+
+  //receive an event from the client side
+  socket.on("join", ({ name, room }) => {
+    // console.log("name, room", name, room);
+  });
 
   socket.on("disconnect", () => {
     console.log("User had left!");
---
+++ users.js
@@ -0,0 +1,53 @@
+const users = [];
+
+//add user
+const addUser = ({ id, name, room }) => {
+  //change user's data to a lowercase string without space
+  name = name.trim().toLowerCase();
+  room = room.trim().toLowerCase();
+
+  //check if there is an existing user
+  const existingUser = users.find(
+    (user) => user.room === room && user.name === name
+  );
+
+  if (existingUser) {
+    return { error: "Username is taken" };
+  }
+
+  //if no existing user, then create a new user
+  const user = { id, name, room };
+  users.push(user);
+
+  //to know which user was pushed
+  return { user };
+};
+
+//remover user
+const removerUser = (id) => {
+  //find user with that specific id
+
+  const index = users.findIndex((user) => {
+    user.id === id;
+  });
+
+  // if there is a matched index, use splice to remove the user and [0] is to return it that removed user
+  if (index !== -1) {
+    return users.splice(index, 1)[0];
+  }
+};
+
+//get user
+const getUser = (id) =>
+  users.find((user) => {
+    user.id === id;
+  });
+
+//get users in room
+const getUsersInRoom = (room) => {
+  users.filter((user) => {
+    user.room === room;
+  });
+};
+
+module.exports = { addUser, removerUser, getUser, getUsersInRoom };
